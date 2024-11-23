9ba2c286b4aad5c6847bb1eabdf3cb7fdfc0c232
Deepak Raushan <deepaklearner@gmail.com>
2024-11-23 14:18:02 -0500

    s

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,6 +10,9 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200,800))
         pygame.display.set_caption("Alien Invasion")
 
+        # set background color
+        self.bg_color =(230,230,230)
+
     def run_game(self):
         """ Start the main loop for the game"""
         while True:
@@ -17,6 +20,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
+
+            # make the mist recently screen drawn visible
             pygame.display.flip()
 
 if __name__=='__main__':
