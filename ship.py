55d6db489ee01d75d2113e3cd8c07d8c24b93961
Deepak Raushan <deepaklearner@gmail.com>
2024-11-24 08:13:34 -0500

    u

--- ship.py
+++ ship.py
@@ -7,6 +7,9 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
+        # Movement flag
+        self.moving_right = False
+
         # Load the ship image
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -18,4 +21,9 @@ class Ship:
     def blitme(self):
         """draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """ Update the ship position based on movement flag"""
+        if self.moving_right:
+            self.rect.x += 1
         
