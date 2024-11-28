9b0e782b19ddae9b16e245403c34f223ba87df15
ankitarani93 <84470513+ankitarani93@users.noreply.github.com>
2024-11-28 09:37:48 -0500

    u

--- setttings.py
+++ setttings.py
@@ -4,8 +4,8 @@ class Settings:
         """Initialize the game settings"""
 
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
 
         self.bg_color = (230,230,230)
         self.ship_speed = 1.5
