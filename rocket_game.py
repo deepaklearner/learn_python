73ae150ab269813aa7d26160799251c1a7f9619c
ankitarani93 <84470513+ankitarani93@users.noreply.github.com>
2024-12-01 09:07:38 -0500

    d

--- rocket_game.py
+++ rocket_game.py
@@ -3,10 +3,11 @@ import sys
 
 if __name__ =="__main__":
     pygame.init()
-    
-    pygame.display.set_mode((800,600))
 
+    screen = pygame.display.set_mode((800,600))
     pygame.display.set_caption("rocket game")
+    screen.fill(color=(100,100,100))
+
     while(True):
         pygame.display.flip()
         for event in pygame.event.get():
